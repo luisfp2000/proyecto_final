5844a5b01a9dc0aeabb3c90c873eed911432f278
luisfp2000 <luisfp2000@gmail.com>
2023-08-15 23:46:40 -0600

    api changes

---
+++ Refactor/refactor_salarios/ds_salaries_predictions/ds_salaries_predictions/api/main.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+from fastapi import FastAPI
+from predictor.predict import ModelPredictor
+from starlette.responses import JSONResponse
+
+from .models.models import DSSalariesPrediction
+
+# Add the parent directory to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+
+
+app = FastAPI()
+
+
+@app.get('/', status_code=200)
+async def healthcheck():
+    return 'DS Salaries Predictor is all ready to go!'
+
+
+@app.post('/predict')
+def extract_name(ds_salaries_features: DSSalariesPrediction):
+    predictor = ModelPredictor("Refactor\refactor_salarios\ds_salaries_predictions\ds_salaries_predictions\models\linear_regression_output.pkl")
+    X = [ds_salaries_features.company_location,
+         ds_salaries_features.company_location,
+         ds_salaries_features.employee_residence,
+         ds_salaries_features.employment_type,
+         ds_salaries_features.employee_residence,
+         ds_salaries_features.job_title]
+    prediction = predictor.predict([X])
+    return JSONResponse(f"Resultado predicción: {prediction}")
---
+++ Refactor/refactor_salarios/ds_salaries_predictions/ds_salaries_predictions/api/models/models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class DSSalariesPrediction (BaseModel):
+    """
+    Representa los atributos para poder realizar el pronostico del precio de la casa
+    Attributes:
+    experience_level
+    employment_type
+    job_title
+    employee_residence
+    company_location
+    company_size
+    """
+    experience_level: str
+    employment_type:str
+    job_title:str
+    employee_residence:str
+    company_location:str
+    company_size:str
+
+    
