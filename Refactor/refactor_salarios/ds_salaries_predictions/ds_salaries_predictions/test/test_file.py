b720d9df609d8e463c033807e9aa17bddae2f6c9
luisfp2000 <luisfp2000@gmail.com>
2023-08-16 17:56:06 -0600

    commit

--- Refactor/refactor_salarios/ds_salaries_predictions/ds_salaries_predictions/test/test_file.py
+++ Refactor/refactor_salarios/ds_salaries_predictions/ds_salaries_predictions/test/test_file.py
@@ -2,6 +2,26 @@ import os
 import pytest
 from load.load_data import DataRetriever
 
+
+def test_csv_file_existence():
+    """
+    Test case to check if the CSV file exists.
+    """
+  
+    # Provide the path to your CSV file that needs to be tested
+    # refactored folder
+    REFACTORED_DIRECTORY = "C:/Users/luis.fernandez.COPPEL/LFPGit/proyectofinal/Refactor/refactor_salarios/ds_salaries_predictions"
+
+    os.chdir(REFACTORED_DIRECTORY)
+    csv_file_path = "./data/ds_salaries.csv"
+
+    DATASETS_DIR = "./data/"
+
+    # Call the function to check if the CSV file exists
+    file_exists = does_csv_file_exist(csv_file_path)
+    # Use Pytest's assert statement to check if the file exists
+    assert file_exists == True, f"The CSV file at '{csv_file_path}' does not exist."
+
 def does_csv_file_exist(file_path):
     """
     Check if a CSV file exists at the specified path.
@@ -15,30 +35,7 @@ def does_csv_file_exist(file_path):
     return os.path.isfile(file_path)
 
 
-def test_csv_file_existence():
-    """
-    Test case to check if the CSV file exists.
-    """
-    # Provide the path to your CSV file that needs to be tested
-    # os.chdir('C:/Users/luis.fernandez.COPPEL/LFPGit/proyectofinal/Refactor/refactor_salarios/ds_salaries_predictions/ds_salaries_predictions')
-
-    csv_file_path = "./data/ds_salaries.csv"
-
-    DATASETS_DIR = './data/'
-    FILE_PATH = "C:/Users/luis.fernandez.COPPEL/LFPGit/proyectofinal/Refactor/refactor_salarios/ds_salaries_predictions/ds_salaries_predictions/data/ds_salaries.csv"
-    
-
-    data_retriever = DataRetriever(FILE_PATH, DATASETS_DIR)
-    data_retriever.retrieve_data()
-
-    # Call the function to check if the CSV file exists
-    file_exists = does_csv_file_exist(csv_file_path)
-
-    # Use Pytest's assert statement to check if the file exists
-    if file_exists:
-        f"The CSV file at '{csv_file_path}' does not exist."
-
-
 if __name__ == "__main__":
     # Run the test function using Pytest
     pytest.main([__file__])
+
